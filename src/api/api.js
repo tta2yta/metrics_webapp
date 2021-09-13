48ccc9efb627653e175cdfcb583ecaa0fe9b52e7
tta2yta <tta2yta@gmail.com>
2021-09-13 11:51:29 +0300

    Add getCountries api

--- src/api/api.js
+++ src/api/api.js
@@ -1,3 +1,14 @@
-export const getCountries = async ()=>{
-    const response = await fetch ("https://restcountries.com/v3/all")
-}
+const getCountries = async () => {
+  const response = await fetch('https://restcountries.com/v3/all', {
+    method: 'GET',
+    headers: {
+      'content-type': 'application/json',
+    },
+    mode: 'cors',
+  }).then((res) => res.json())
+    .catch((err) => err);
+
+  return response;
+};
+
+export default getCountries;
