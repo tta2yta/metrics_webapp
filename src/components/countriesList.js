0f3b250203bdcd8831046114f4d5b7e8ce6b585a
tta2yta <tta2yta@gmail.com>
2021-09-13 14:02:30 +0300

    Add provide to index.js file

--- src/components/countriesList.js
+++ src/components/countriesList.js
@@ -1,7 +1,13 @@
-const countriesList = () => (
-  <div className="countries-list">
-    countriesList
-  </div>
-);
+import { useSelector } from 'react-redux';
+
+const countriesList = () => {
+  const countries = useSelector((state) => state.countries);
+  console.log(countries);
+  return (
+    <div className="countries-list">
+      countriesList
+    </div>
+  );
+};
 
 export default countriesList;
