28865caea8a92d38479486cb1ac8cf8271cc58c9
tta2yta <tta2yta@gmail.com>
2021-09-15 00:58:48 +0300

    Add styles

--- src/components/companiesList.js
+++ src/components/companiesList.js
@@ -1,22 +1,33 @@
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchCompanies } from '../redux/actions/companiesAction';
+import './companies.css';
 
 const companiesList = () => {
   const companies = useSelector((state) => state.companies);
   const dispatch = useDispatch();
+  const bg = { 'bg-1': 'bg-1', 'bg-2': 'bg-2' };
+  let kkk = '';
   console.log(companies);
 
   useEffect(() => {
     dispatch(fetchCompanies());
   }, []);
   return (
-    <div className="companies-list">
-      {companies.map((item) => (
-        <div key={item.symbol}>
-          {' '}
-          <li>{item.symbol}</li>
-          <li>{item.price}</li>
+    <div className="companies-list-main">
+      {companies.map((item, index) => (
+        <div className="companies-list" key={item.symbol}>
+          {console.log(index)}
+          {(() => {
+            if (index % 2 === 0 && index !== 0) {
+              kkk = bg['bg-1'];
+              console.log(kkk);
+            }
+          })()}
+          <div className="list-items">
+            <div>{item.symbol}</div>
+            <div>{item.price}</div>
+          </div>
         </div>
 
       ))}
