75f383c31270cbfb0fe9ada3e4d189aa3eb96ec8
tta2yta <tta2yta@gmail.com>
2021-09-16 01:47:26 +0300

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
 import CompaniesStock from './components/CompaniesStock';
+import CompaniesDetails from './components/companiesDetails';
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: <CompaniesStock />,
+  },
+  {
+    path: '/details',
+    name: 'Details',
+    component: <CompaniesDetails />,
+  },
+];
 
 function App() {
   return (
     <div className="App">
-      <CompaniesStock />
+      <Router>
+        <Switch>
+          {routes.map(({ path, component }) => (
+            <Route path={path} exact key={path}>{component}</Route>
+          ))}
+        </Switch>
+      </Router>
+
     </div>
   );
 }
--- src/components/companiesList.js
+++ src/components/companiesList.js
@@ -1,15 +1,12 @@
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchCompanies } from '../redux/actions/companiesAction';
 import './companies.css';
 
 const companiesList = () => {
   const companies = useSelector((state) => state.companies);
   const dispatch = useDispatch();
-  const bg = { 'bg-1': 'bg-1', 'bg-2': 'bg-2' };
-  let kkk = '';
-  console.log(companies);
-
   useEffect(() => {
     dispatch(fetchCompanies());
   }, []);
@@ -20,16 +17,12 @@ const companiesList = () => {
         {companies.map((item, index) => (
           <div className="companies-list" key={item.symbol}>
             {console.log(index)}
-            {(() => {
-              if (index % 2 === 0 && index !== 0) {
-                kkk = bg['bg-1'];
-                console.log(kkk);
-              }
-            })()}
-            <div className="list-items">
-              <div className="symbol">{item.symbol}</div>
-              <div className="price">{item.price}</div>
-            </div>
+            <Link to="/details">
+              <div className="list-items">
+                <div className="symbol">{item.symbol}</div>
+                <div className="price">{item.price}</div>
+              </div>
+            </Link>
           </div>
 
         ))}
